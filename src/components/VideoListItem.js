3cc9a9f745e9f0c10c4bd0d16d670089856b1647
Marlon Decosta <marlondecosta@Embodied-Mac-Pro-Beta.local>
2018-08-07 03:12:55 -0400

    first commit

---
+++ src/components/VideoListItem.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const VideoListItem = ({ video, onVideoSelect }) => {
+  
+  const imageUrl = video.snippet.thumbnails.default.url 
+  const vidTitle = video.snippet.title 
+
+  return (
+    <li onClick={() => onVideoSelect(video)} className='list-group-item'>
+      <div className="video-list media">
+        <div className="media-left">
+          <img src={imageUrl} alt={vidTitle} className="media-object"/>
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">{vidTitle}</div>
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export default VideoListItem
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
   module: {
     loaders: [
       {
-        exclude: /node_modules/,
         loader: 'babel',
         query: {
           presets: ['react', 'es2015', 'stage-1']
